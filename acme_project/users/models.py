38d5104ca14ad47f2259e2ce334a738ebf6f174f
Владлен Ким <vladlen-kim90@yandex.ru>
2023-06-23 21:39:32 +0400

    Проект

---
+++ acme_project/users/models.py
@@ -0,0 +1,8 @@
+# users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class MyUser(AbstractUser):
+    bio = models.TextField('Биография', blank=True)
+# Create your models here.
